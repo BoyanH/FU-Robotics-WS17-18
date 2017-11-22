3e8e383ecdef311545e4b09a65852815307129e8
Boyan Hristov <bhristov96@gmail.com>
2017-11-22 22:37:52 +0100

    merged latest work from team

--- Homework5/scripts/heading_straight.py
+++ Homework5/scripts/heading_straight.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import rospy
+import sys
 import math
 import numpy as np
 from std_msgs.msg import Float32, Int16
@@ -9,51 +10,60 @@ from std_msgs.msg import Float32, Int16
 experiment_started = False
 squared_errors = []
 times = []
-Kp = 0.6
+Kp = -5
+firstCall = True
+desired_angle = 0
 
 
-def get_calibrated_steering(angle):
-    return 80
+def get_calibrated_steering():
+    return 100
 
 
 def yaw_callback(yaw_msg):
     global squared_errors
+    global desired_angle
+    global firstCall
 
-    rospy.loginfo(yaw_msg)
-    desired_angle = 0  # we want to go straight
+    if firstCall:
+        desired_angle = yaw_msg.data + 20
+        firstCall = False
+
     current_angle = yaw_msg.data
-    calibrated_angle = get_calibrated_steering(90)
+    #rospy.loginfo('current: {}; desired: {}'.format(current_angle, desired_angle))
+    calibrated_angle = get_calibrated_steering()
     squared_error = (desired_angle - current_angle) ** 2
+    pub_squared_error.publish(squared_error)
     squared_errors.append(squared_error)
-    times.append(rospy.get_time())
 
-    pub_steering.publish(Kp * (desired_angle - current_angle) + calibrated_angle)
+    current_time = rospy.get_time()
+    times.append(current_time)
+
+    if current_time - times[0] >= 13:
+        squared_errors_avg = np.array(squared_errors).mean()
+        pub_speed.publish(0)
+        rospy.loginfo('average squared error: {}'.format(squared_errors_avg))
+        rospy.signal_shutdown("done")
+
+    steering = Kp * (desired_angle - current_angle) + calibrated_angle
+    rospy.loginfo(steering)
+    pub_steering.publish(steering)
     start_experiment()
 
 
 def start_experiment():
     global experiment_started
-    rate = rospy.Rate(10)  # 10hz
 
     if experiment_started:
         return
 
     experiment_started = True
 
-    rospy.sleep(0.5)
-    for i in range(100):
-        rospy.loginfo(' go straight')
-        pub_speed.publish(-100)
-        rate.sleep()
-    for i in range(10):
-        pub_speed.publish(0)
-        rate.sleep()
-
-    # experiment finished, draw plot
-    # draw_plot()
-
-# def draw_plot():
-#     figure = plt.figure()
+    pub_stop_start.publish(0)
+    rospy.sleep(1)
+    pub_steering.publish(get_calibrated_steering())
+    rospy.sleep(1)
+    pub_speed.publish(-300)
+    rospy.sleep(1)
 
 
 # --- main ---
@@ -63,7 +73,10 @@ rospy.loginfo('started')
 
 rospy.Subscriber("/model_car/yaw", Float32, yaw_callback, queue_size=100)
 
+pub_squared_error = rospy.Publisher("/squared_error", Int16, queue_size=100)
 pub_speed = rospy.Publisher("/manual_control/speed", Int16, queue_size=100)
+pub_stop_start = rospy.Publisher("/manual_control/stop_start", Int16, queue_size=100)
 pub_steering = rospy.Publisher("/manual_control/steering", Int16, queue_size=100)
+rospy.sleep(1)
 
 rospy.spin()
